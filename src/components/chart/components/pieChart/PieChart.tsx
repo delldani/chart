bde95e42162d11f5aa045a482680de75a4a2f4e9
gordongecco <delldani001@gmail.com>
2022-12-09 09:14:28 +0100

    ref

--- src/components/chart/components/pieChart/PieChart.tsx
+++ src/components/chart/components/pieChart/PieChart.tsx
@@ -5,7 +5,7 @@ import {
   useEffectOnce,
   makeSlice,
   percentToDegree,
-  makeStepByStepSlices
+  makeStepByStepSlices,
 } from "../../helper";
 import styles from "./PieChart.module.css";
 import { PiePrecentType } from "../../chart";
@@ -19,87 +19,100 @@ interface PieChartProps {
 export const PieChart = (props: PieChartProps) => {
   const { piePrecent } = props;
 
-  let stepByStepSlices: number[]  =  makeStepByStepSlices(piePrecent);
+  let stepByStepSlices: number[] = makeStepByStepSlices(piePrecent);
   const activeSliceArray = Array(piePrecent.length).fill(null);
-  const slices = React.useRef<Path2D[]>(piePrecent.map(item=>new Path2D()));
-  let canvas:HTMLCanvasElement | null = null;
+  const slices = React.useRef<Path2D[]>(piePrecent.map((item) => new Path2D()));
+  let canvas: HTMLCanvasElement | null = null;
   const ctxRef = React.useRef<CanvasRenderingContext2D | null | undefined>(
     null
   );
 
   useEffectOnce(() => {
-    canvas= document.querySelector("canvas");
+    canvas = document.querySelector("canvas");
     ctxRef.current = canvas?.getContext("2d");
     const ctx = ctxRef.current;
 
-    if(ctx){
-      slices.current.forEach((slice,index) => {
-        
-        const startDegree = percentToDegree(stepByStepSlices[index]);
-        const endDegree = slices.current.length-1 === index ? 360 :  percentToDegree(stepByStepSlices[index+1]);
-        makeSlice(slice,CANVAS_WIDTH/2,CANVAS_WIDTH/2, startDegree, endDegree, RADIUS);
-        //átlátszó legyen a vonal
-        ctx.strokeStyle="rgba(0, 0, 0, 0)";
-        ctx.stroke(slice);
-        (ctx as CanvasFillStrokeStyles).fillStyle = piePrecent[index].color;
-        ctx.fill(slice);
-      });
-    }
-  
+    slices.current.forEach((slice, index) => {
+      drawSlices(ctx, index, slice, RADIUS);
+    });
   });
+
+  const drawSlices = (
+    ctx: CanvasRenderingContext2D | null | undefined,
+    index: number,
+    slice: Path2D,
+    radius: number
+  ) => {
+    if (ctx) {
+      const startDegree = percentToDegree(stepByStepSlices[index]);
+      const endDegree =
+        slices.current.length - 1 === index
+          ? 360
+          : percentToDegree(stepByStepSlices[index + 1]);
+      makeSlice(
+        slice,
+        CANVAS_WIDTH / 2,
+        CANVAS_WIDTH / 2,
+        startDegree,
+        endDegree,
+        radius
+      );
+      //átlátszó legyen a vonal
+      ctx.strokeStyle = "rgba(0, 0, 0, 0)";
+      ctx.stroke(slice);
+      (ctx as CanvasFillStrokeStyles).fillStyle = piePrecent[index].color;
+      ctx.fill(slice);
+    }
+  };
 
   const pieGradient = makePieGradient(piePrecent);
 
   const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
     const ctx = ctxRef.current;
 
-    if(ctx){
-      let changed:null |number = null;
+    if (ctx) {
+      let changed: null | number = null;
       let isActive = false;
-    slices.current.map((slice,index)=>{
+      slices.current.map((slice, index) => {
         const isPointInPath = ctx.isPointInPath(
           slice,
           e.nativeEvent.offsetX,
           e.nativeEvent.offsetY
-          );
-          if(isPointInPath ){
-            isActive = true;
-            if(activeSliceArray[index] === null){
-                // activeSlice.fill(null);
-                activeSliceArray[index] = 1;
-                changed = index;
-              }
-          }else{
-            activeSliceArray[index] = null;
+        );
+        if (isPointInPath) {
+          isActive = true;
+          if (activeSliceArray[index] === null) {
+            // activeSlice.fill(null);
+            activeSliceArray[index] = 1;
+            changed = index;
           }
-        });
-        if(changed !== null || !isActive){
-          if(ctx){
-            ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_WIDTH);
-            slices.current.forEach((item,ind,arr)=>arr[ind] = new Path2D());
-            slices.current.forEach((slice,index) => {
-              let radius = RADIUS;
-              if(changed === index){
-                radius = RADIUS+20;
-              } ;
-              if(!isActive){
-                radius = RADIUS;
-              };
-              const startDegree = percentToDegree(stepByStepSlices[index]);
-              const endDegree = slices.current.length-1 === index ? 360 :  percentToDegree(stepByStepSlices[index+1]);
-              makeSlice(slice,CANVAS_WIDTH/2,CANVAS_WIDTH/2, startDegree, endDegree,radius);
-              //átlátszó legyen a vonal
-              ctx.strokeStyle="rgba(0, 0, 0, 0)";
-              ctx.stroke(slice);
-              (ctx as CanvasFillStrokeStyles).fillStyle = piePrecent[index].color;
-              ctx.fill(slice);
-            });
-          }
+        } else {
+          activeSliceArray[index] = null;
         }
-      };
+      });
+      if (changed !== null || !isActive) {
+        if (ctx) {
+          ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_WIDTH);
+          slices.current.forEach((item, ind, arr) => (arr[ind] = new Path2D()));
+          slices.current.forEach((slice, index) => {
+            let radius = RADIUS;
+            if (changed === index) {
+              radius = RADIUS + 20;
+            }
+            if (!isActive) {
+              radius = RADIUS;
+            }
+            drawSlices(ctx, index, slice, radius);
+          });
+        }
+      }
+    }
   };
 
-  if(!checkPercentAre100(piePrecent)){console.warn('nem adnak 100% ot ki a szeletek'); return null};
+  if (!checkPercentAre100(piePrecent)) {
+    console.warn("nem adnak 100% ot ki a szeletek");
+    return null;
+  }
 
   return (
     <>
